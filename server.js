8c5437c131a83b77c81e2040fda01b58c7707ada
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-04-25 11:19:57 -0400

    created route to accounts view

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
+const accountApi = require('./api/accountApi.js');
 const express = require('express');
 const app = express();
 const methodOverride = require("method-override");
 
 app.use(express.urlencoded());
-app.use(methodOverride('_method'))
-app.use(express.static(__dirname+"/public"))
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname+"/public"));
 
 app.set("view engine", "hbs");
 
 app.get('/', (req,res) => {
-    res.send("you've found me")
+    res.redirect("/accounts");
 })
+
+app.get('/accounts', (req,res) => {
+    accountApi.getAllAccounts()
+        .then(accounts => {
+            res.render("accounts/accounts")
+        }) 
+}) 
 
 const PORT = process.env.PORT || 3000 
 app.listen(PORT, () => {
