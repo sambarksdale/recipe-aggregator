b0bb167ee1a7d38160d65cb02dca04bb990bc784
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-04-29 12:52:00 -0400

    fixed count incrament in module creation

--- public/js/new-recipe.js
+++ public/js/new-recipe.js
@@ -10,23 +10,20 @@ function newModule(){
     $('#addIngredient').before(`<div class="new-recipe-form-module" id="ingredient-module-${count}"></div>`)
     $(`#ingredient-module-${count}`).append(`<input type="text" name="ingredients" value="" id="ingredient-${count}"/>`)
     $(`#ingredient-module-${count}`).append(`<input type="button" value="X" id="delete-${count}" />`)
-    console.log(count);
     $(`#delete-${count}`).on('click', function(){
         console.log($(this).parent())
         $(this).parent().remove();
-        // console.log(document.getElementById(`ingredient-module-${count -1}`))
-        //$('div').filter(document.getElementById(`ingredient-module-${count-1}`)).remove();
     })
 }
 
 function deleteModule(n) {
-        $('div').filter(document.getElementById(`ingredient-module-${n}`)).remove();
+    $(this).parent().remove();;
 }
 
 function createNewModule() {
     $('#addIngredient').on('click', function(){
         newModule();
-        count += count++
+        count = count + 1
     })
 }
 
