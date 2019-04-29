de18788834c125372d1de932347c5d74d7f27d82
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-04-29 13:12:33 -0400

    added new grocey list form

--- public/js/new-recipe.js
+++ public/js/new-recipe.js
@@ -1,23 +1,12 @@
 let count = 1
 
-// let module = `<div class="new-recipe-form-module" id="ingredient-module-${count}">
-// <input type="text" name="ingredients" value="" id="ingredient-${count}"/>
-// <input type="button" value="X" id="delete-${count}" />
-// </div>`
-
-
 function newModule(){
     $('#addIngredient').before(`<div class="new-recipe-form-module" id="ingredient-module-${count}"></div>`)
     $(`#ingredient-module-${count}`).append(`<input type="text" name="ingredients" value="" id="ingredient-${count}"/>`)
     $(`#ingredient-module-${count}`).append(`<input type="button" value="X" id="delete-${count}" />`)
     $(`#delete-${count}`).on('click', function(){
-        console.log($(this).parent())
         $(this).parent().remove();
     })
-}
-
-function deleteModule(n) {
-    $(this).parent().remove();;
 }
 
 function createNewModule() {
@@ -27,11 +16,7 @@ function createNewModule() {
     })
 }
 
-
-
-
-
- $(function(){
+$(function(){
      createNewModule();
 })
 
