afb7667548eed482cdd4f3f242fd96606bd62ed9
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-05-01 17:31:10 -0400

    updated css

--- public/js/delete-module.js
+++ public/js/delete-module.js
@@ -8,7 +8,7 @@ function addListItem() {
     $('#new-list-item').on('click', function() {
         console.log("asdfasdfas")
         $('#new-list-item').before('<div class="list-item"></div>')
-        $('.list-item').last().append('<input type="text" name="list" value="" />')
+        $('.list-item').last().append('<input type="text" name="list" value="" class="form-text-input"/>')
         $('.list-item').last().append('<input type="button" value="X" class="delete-item" />')
     })
 }
--- public/js/new-input-module.js
+++ public/js/new-input-module.js
@@ -3,7 +3,7 @@ let count = 1
 function newRecipeModule(){
     let name = $('label').filter(document.getElementById('set-name')).attr('for')
     $('#addInput').before(`<div class="new-recipe-form-module" id="input-module-${count}"></div>`)
-    $(`#input-module-${count}`).append(`<input type="text" name="${name}" value="" id="ingredient-${count}"/>`)
+    $(`#input-module-${count}`).append(`<input type="text" name="${name}" value="" id="ingredient-${count}" class="form-text-input"/>`)
     $(`#input-module-${count}`).append(`<input type="button" value="X" id="delete-${count}" />`)
     $(`#delete-${count}`).on('click', function(){
         $(this).parent().remove();
